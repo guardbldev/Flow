d598e511cb88dfc3a3febe403078452f024b399f
guardbl <guardblroblox@gmail.com>
2026-01-05 13:10:26 +0000

    Delete Flow directory

--- Flow/flow_parser_switch_snippet.cpp
+++
@@ -1,9 +0,0 @@
-switch (tk.keyword) {
-    case FlowKeyword::Text:         // text node/element
-    case FlowKeyword::Button:       // button element
-    case FlowKeyword::Card:         // card/container
-    case FlowKeyword::Style:        // style block
-    case FlowKeyword::Theme:        // theme configuration
-    // Continue for each feature exactly as above, no numbers, only English description
-    break;
-}
--- Flow/src/flow_parser_bridge.ts
+++
@@ -1,6 +0,0 @@
-import { spawnSync } from 'child_process';
-
-export function getFlowDiagnostics(code: string) {
-    const result = spawnSync('./flow-parser', [], { input: code, encoding: 'utf8' });
-    return result.stdout;
-}
